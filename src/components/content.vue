d32b0a60b576ef427ef5a7f0638e02e65bc44fbd
zhangqiang <zhangqiang@xiongmao1718.com>
2018-05-11 17:46:52 +0800

    Initial commit

---
+++ src/components/content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header></Header>
+    <div>我是内容</div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
+document.title = '内容页'
+
+export default {
+  name: 'contenter',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
---
+++ src/components/footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>我是底部</div>
+</template>
+
+<script>
+export default {
+  name: 'footerr'
+}
+</script>
